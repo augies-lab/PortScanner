0d87b2b031324012e2c0e4a5e71fe5bfa9990f39
augies-lab <184285aug@gmail.com>
2019-09-24 18:38:35 -0700

    Import Modules

--- Tests/SocketHandlingUnitTests.py
+++ Tests/SocketHandlingUnitTests.py
@@ -1,1 +1,5 @@
 # 09/21: SocketHandling unit tests go in here!
+
+# import importlib
+
+# importlib.import_module(PortScanner.SocketHandling)
--- Tests/UserInputUnitTests.py
+++ Tests/UserInputUnitTests.py
@@ -1,1 +1,5 @@
 # 09/21: UserInput unit tests go in here!
+
+# import importlib
+
+# importlib.import_module(PortScanner.UserInput)
